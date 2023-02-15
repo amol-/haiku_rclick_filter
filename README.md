1ee23f9f103524762f04d2df850e69018c80055a
Alessandro Molina <amol@turbogears.org>
2023-02-15 01:16:01 +0000

    Add scrolling support

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # haiku_rclick_filter
-Input Filter for Haiku that turns CTRL+CLICK to a right click.
-Helpful for touchpads missing right click.
+Input Filter for Haiku that turns CTRL+CLICK to a right click
+and CTRL+MOVE into scrolling.
+Helpful for touchpads missing right click and two finger scrolling.
 
 # Install
 ```
@@ -9,4 +10,6 @@ make install
 ```
 
 # Usage
+
 After install keeping CTRL pressed while you click should turn your left clicks to right clicks.
+Keep CTRL pressed while you move the mouse to trigger a vertical scroll event.
--- rclick_ifilter.cpp
+++ rclick_ifilter.cpp
@@ -5,10 +5,19 @@
 
 class RClickInputFilter : public BInputServerFilter
 {
+private:
+	bool scrollingInProgress;
+	BPoint previousMousePosition;
 public:
+	RClickInputFilter();
     virtual status_t InitCheck();
     virtual filter_result Filter(BMessage *message, BList *outList);
 };
+
+RClickInputFilter::RClickInputFilter()
+: scrollingInProgress(false)
+{
+}
 
 status_t RClickInputFilter::InitCheck()
 {
@@ -17,24 +26,47 @@ status_t RClickInputFilter::InitCheck()
 
 filter_result RClickInputFilter::Filter(BMessage *message, BList *outList)
 {
-    if (message->what != B_MOUSE_DOWN)
+    if ((message->what != B_MOUSE_DOWN) && (message->what != B_MOUSE_MOVED))
         return B_DISPATCH_MESSAGE;
 
-    int32 buttons;
-    if (message->FindInt32("buttons", &buttons) != B_OK)
-        return B_DISPATCH_MESSAGE;
+	int32 modifiers;
+	if (message->FindInt32("modifiers", &modifiers) != B_OK) {
+		scrollingInProgress = false;
+		return B_DISPATCH_MESSAGE;
+	}
 
-    if (buttons != B_PRIMARY_MOUSE_BUTTON)
-        return B_DISPATCH_MESSAGE;
+	if (!(modifiers & B_CONTROL_KEY)) {
+		scrollingInProgress = false;
+		return B_DISPATCH_MESSAGE;
+	}
 
-    int32 modifiers;
-    if (message->FindInt32("modifiers", &modifiers) != B_OK)
-        return B_DISPATCH_MESSAGE;
+	if (message->what == B_MOUSE_DOWN) {
+		// Turn CTRL+CLICK into RIGHT CLICK
+		int32 buttons;
+		if (message->FindInt32("buttons", &buttons) != B_OK)
+			return B_DISPATCH_MESSAGE;
 
-    if (!(modifiers & B_CONTROL_KEY))
-        return B_DISPATCH_MESSAGE;
+		if (buttons != B_PRIMARY_MOUSE_BUTTON)
+			return B_DISPATCH_MESSAGE;
 
-    message->ReplaceInt32("buttons", B_SECONDARY_MOUSE_BUTTON);
+		message->ReplaceInt32("buttons", B_SECONDARY_MOUSE_BUTTON);
+	}
+	else if (message->what == B_MOUSE_MOVED) {
+		// Turn CTRL+MOVE into SCROLLING
+		BPoint newMousePosition;
+		if (message->FindPoint("where", &newMousePosition) != B_OK)
+			return B_DISPATCH_MESSAGE;
+
+		if (!scrollingInProgress) {
+			scrollingInProgress = true;
+			previousMousePosition = newMousePosition;
+			return B_DISPATCH_MESSAGE;
+		}
+
+		float deltay = newMousePosition.y - previousMousePosition.y;
+		message->what = B_MOUSE_WHEEL_CHANGED;
+		message->AddFloat("be:wheel_delta_y", deltay);
+	}
     return B_DISPATCH_MESSAGE;
 }
 
